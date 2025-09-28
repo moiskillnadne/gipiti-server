b5f9cc8613e9ee1c53b16d3227f42b0e7d1dbaf3
Viktor Riabkov <vitya.ryabkov@gmail.com>
2025-09-28 21:38:57 +0500

    health check

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.get("/health-check", (req, res) => {
-  res.status(200).send("OK")
+  res.status(200).send(`[${process.env.ENV}] OK`)
 })
 
 app.listen(port, () => {
