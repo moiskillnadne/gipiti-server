60e7088c86542b9817c4b39aa9926740c9dd8d63
Viktor Riabkov <vitya.ryabkov@gmail.com>
2025-09-28 21:38:01 +0500

    Health-check

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.get("/health-check", (req, res) => {
+  res.status(200).send("OK")
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
